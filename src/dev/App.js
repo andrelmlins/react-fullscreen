bfe17de9a56c19f15f4cb8c843892b5ae6f25826
André Lins <andrelucas01@hotmail.com>
2019-08-18 19:28:05 -0300

    Correction of lint

--- src/dev/App.js
+++ src/dev/App.js
@@ -35,6 +35,13 @@ const style = {
   image: {
     cursor: 'pointer',
     width: '50%'
+  },
+  buttonImage: {
+    border: 'none',
+    background: 'transparent',
+    '&:focus': {
+      outline: 'none'
+    }
   }
 };
 
@@ -69,13 +76,14 @@ const App = () => {
       </FullScreenComponent>
       <FullScreenComponent>
         {({ ref, onToggle }) => (
-          <img
-            alt="Fullscreen"
-            ref={ref}
-            src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2"
-            onClick={() => onToggle()}
-            style={style.image}
-          />
+          <button style={style.buttonImage} onClick={() => onToggle()}>
+            <img
+              alt="Fullscreen"
+              ref={ref}
+              src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2"
+              style={style.image}
+            />
+          </button>
         )}
       </FullScreenComponent>
     </div>
