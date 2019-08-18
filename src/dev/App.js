db2a7f9347503de586f5dbd7df2de905d2b580bf
André Lins <andrelucas01@hotmail.com>
2019-08-18 19:23:04 -0300

    Refactoring component to hooks

---
+++ src/dev/App.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import FullScreenComponent from '../lib';
+
+const style = {
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    flexFlow: 'column wrap',
+    justifyContent: 'center',
+    width: '100%'
+  },
+  title: {
+    color: '#1f2041',
+    textTransform: 'uppercase'
+  },
+  screen: {
+    alignItems: 'center',
+    backgroundColor: '#417b5a',
+    display: 'flex',
+    flexFlow: 'column wrap',
+    height: 120,
+    justifyContent: 'center',
+    margin: 10,
+    width: 300
+  },
+  button: {
+    backgroundColor: '#4b3f72',
+    border: 0,
+    borderRadius: 4,
+    color: '#e9d2c0',
+    marginBottom: 5,
+    minWidth: 120,
+    padding: 5
+  },
+  image: {
+    cursor: 'pointer',
+    width: '50%'
+  }
+};
+
+const App = () => {
+  return (
+    <div style={style.container}>
+      <h1 style={style.title}>React Fullscreen</h1>
+      <div>
+        <a href="https://github.com/andrelmlins/react-fullscreen">
+          <img
+            alt="GitHub stars"
+            src="https://img.shields.io/github/stars/andrelmlins/react-fullscreen.svg?style=social"
+          />
+        </a>
+      </div>
+      <FullScreenComponent
+        onChange={() => console.log('Screen')}
+        onError={() => console.log('Error')}
+      >
+        {({ ref, onRequest, onExit }) => {
+          return (
+            <div ref={ref} style={style.screen}>
+              <button onClick={() => onRequest()} style={style.button}>
+                FullScreen
+              </button>
+              <button onClick={() => onExit()} style={style.button}>
+                Screen
+              </button>
+            </div>
+          );
+        }}
+      </FullScreenComponent>
+      <FullScreenComponent>
+        {({ ref, onToggle }) => (
+          <img
+            alt="Fullscreen"
+            ref={ref}
+            src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2"
+            onClick={() => onToggle()}
+            style={style.image}
+          />
+        )}
+      </FullScreenComponent>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/lib/ReactFullScreen/index.js
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect, memo } from 'react';
+import PropTypes from 'prop-types';
+import screenfull from 'screenfull';
+
+export const FullScreenComponent = ({ children, onChange, onError }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    screenfull.onchange(() => {
+      if (onChange) {
+        onChange();
+      }
+    });
+    screenfull.on('error', event => {
+      if (onError) {
+        onError();
+      }
+    });
+  }, []);
+
+  return children({
+    ref,
+    onToggle: () => {
+      screenfull.toggle(ref.current);
+    },
+    onRequest: () => {
+      screenfull.request(ref.current);
+    },
+    onExit: () => {
+      screenfull.exit(ref.current);
+    }
+  });
+};
+
+FullScreenComponent.propTypes = {
+  children: PropTypes.func,
+  onChange: PropTypes.func,
+  onError: PropTypes.func
+};
+
+export default memo(FullScreenComponent);
