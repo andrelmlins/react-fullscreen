3c1fbbb9e688e15401774c447a330e1143569bf9
André Lins <andrelucas01@hotmail.com>
2019-09-01 15:27:18 -0300

    Remove code smell

--- src/lib/ReactFullScreen/index.js
+++ src/lib/ReactFullScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, memo } from 'react';
+import { useRef, useEffect, memo } from 'react';
 import PropTypes from 'prop-types';
 import screenfull from 'screenfull';
 
